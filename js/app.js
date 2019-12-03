dff0953b6f96b65c1f4cdf0061462081189f9ec6
diogoavilarr <58117980+diogoavilarr@users.noreply.github.com>
2019-12-03 16:06:52 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,13 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyA968HC-hjbeiYDopFmCvJRMwn0cNvxYTQ",
+    authDomain: "teste-6aded.firebaseapp.com",
+    databaseURL: "https://teste-6aded.firebaseio.com",
+    projectId: "teste-6aded",
+    storageBucket: "teste-6aded.appspot.com",
+    messagingSenderId: "709701275132",
+    appId: "1:709701275132:web:3891b90528ec54279e3679",
+    measurementId: "G-4W6ZP3NLBB"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
